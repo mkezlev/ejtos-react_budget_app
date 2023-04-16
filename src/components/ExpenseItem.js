10a7043ec41becaee99f6bba60a56b48d216ade4
mkezlev <mkezlev@gmail.com>
2023-04-16 15:35:05 -0400

    changes made from theia lab

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
+
+
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
@@ -18,6 +20,23 @@ const ExpenseItem = (props) => {
             cost: 10,
         };
 
+  
+
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+
+    }
+
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: -10,
+        };
+
+  
+
         dispatch({
             type: 'ADD_EXPENSE',
             payload: expense
@@ -29,7 +48,8 @@ const ExpenseItem = (props) => {
         <tr>
         <td>{props.name}</td>
         <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td> 
+        <td><button onClick={event=> decreaseAllocation(props.name)}>-</button></td>   
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
